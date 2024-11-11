60c53c6d21cb1d98f8005c14f962735869399f55
nom519 <wasgm519nh@gmail.com>
2024-11-11 14:55:59 +0900

    11/11web-site作成中

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,4 +6,20 @@ $(document).ready(function(){
     $(".sp-nav-list").on("click", function(){
         $(".sp-nav").slideToggle("active");
     });
+
+    $('.carousel').slick({
+		autoplay: false,
+		autoplaySpeed: 5000,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+		arrows: false,
+		dots: true,
+		fade: false,
+    });
+    $('.js-accordion__title').click(function(){
+        $(this).next('.js-accordion__answer').slideToggle();
+        $(this).toggleClass("open");
+    });
 });
+
